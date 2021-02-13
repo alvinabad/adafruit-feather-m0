2ab5e2c660291cf5eba7025e538cd6021d32eb82
Alvin Abad <alvnabad@gmail.com>
2021-02-12 22:56:12 -0800

    Clean up Blink

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -1,41 +1,30 @@
 /*
   Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-  LED_BUILTIN is pin 13
 */
 
-#include "Arduino.h"
-#include <SPI.h>
-#include <WiFi101.h>
+#include <Arduino.h>
 
-#define DELAY  1000
+#ifndef LED_BUILTIN
+    #define LED_BUILTIN 13
+#endif
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-    //Configure pins for Adafruit ATWINC1500 Feather
-    WiFi.setPins(8,7,4,2);
-  
     Serial.begin(115200);
-    while (!Serial);
 
-    Serial.println();
-    Serial.printf("Begin LED LIGHTS: %d\n", LED_BUILTIN);
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+    // only use if there is serial connection
+    //while (!Serial);
+
+    delay(2000);
+
+    Serial.println("Begin LED BLINKER");
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  Serial.print("LED HIGH:");
-  Serial.println(LED_BUILTIN);
-
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(DELAY);
-  
-  Serial.print("LED LOW:");
-  Serial.println(LED_BUILTIN);
-
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(DELAY);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
 }
